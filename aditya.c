194bbbdf25bbfd21cfbc393ad8371387f5911d1d
aditya-sikarwar <122551732+aditya-sikarwar@users.noreply.github.com>
2023-01-12 21:55:32 +0530

    Add files via upload

---
+++ aditya.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+    int radius;
+    float area, perimete;
+radius = 6;
+
+perimeter=2*3.14*radius;
+printf("The perimete of the given circle is %f\n,perimeter);
+
+area= 3.14*radius*radius;
+printf("Area of the Circle = %f square inches\n", area);
+
+return (0);
+}
